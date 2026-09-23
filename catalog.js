3441c43691669d0826173252d38a8222aac3221b
Nicole Sheen Aldaya <sheennicole78@gmail.com>
2026-09-23 11:53:27 +0800

    Add 1-day grace period

--- catalog.js
+++ catalog.js
@@ -1,9 +1,10 @@
-function isValidLoan(daysLate) {
-  return daysLate >= 0;
+function isValidLoan(daysLate){
+return daysLate >= 0;
 }
-
-function calculateLateFee(daysLate, ratePerDay) {
-  return Math.floor(daysLate * ratePerDay);
+function calculateLateFee(daysLate, ratePerDay){
+if (daysLate <= 1){
+return 0;
 }
-
+return Math.floor(daysLate * ratePerDay);
+}
 module.exports = { isValidLoan, calculateLateFee };
